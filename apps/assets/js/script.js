78d8a38fc28d6aea2c9235db7c903b9a8485ddf6
Leonardo da Silva Villa <leosivi@gmail.com>
2024-07-12 01:27:28 -0400

    Update script.js

--- apps/assets/js/script.js
+++ apps/assets/js/script.js
@@ -4,6 +4,8 @@ const form = document.forms['submit-to-google-sheet']
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => console.log('Success!', response))
+        .then(response => {
+          alert("Dados Cadastrados com Sucesso!")
+        })
         .catch(error => console.error('Error!', error.message))
 })
