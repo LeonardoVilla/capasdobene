48d2491f25f2a7f8d4be33445ac06968aadb480d
Leonardo da Silva Villa <leosivi@gmail.com>
2024-08-14 22:36:40 -0400

    k

--- apps/assets/js/scriptnew.js
+++ apps/assets/js/scriptnew.js
@@ -15,6 +15,7 @@ form.addEventListener('submit', e => {
     campo6: document.getElementById('nomecliente').value,
     campo7: document.getElementById('emailcliente').value,
     campo8: document.getElementById('telefonecliente').value,
+    campo9: document.getElementById('dataHora').value,
     // Adicione outros campos conforme necessário
   };
 
